66b090f03e8485f9659b8cdc16c94d719105fd37
Matej <matko05052@gmail.com>
2022-05-17 22:21:21 +0200

    Už to funguje
    Podľa Odin požiadaviek, máme buttony na výber možností a tiež počítadlo ktoré ukáže kto vyhral 5 kôl

--- javascrypt.js
+++ javascrypt.js
@@ -1,5 +1,6 @@
 let moznosti = ['kamen', 'papier', 'noznice']
 
+
 function computerPlay() {
 return moznosti[Math.floor(Math.random()*moznosti.length)]
 }
@@ -8,50 +9,71 @@ return moznosti[Math.floor(Math.random()*moznosti.length)]
 let hrac = 0
 let pocitac = 0
 
-function Kolo() {
-    const vstup = prompt('Zadajte Váš ťah proti počítaču!', 'kamen,papier alebo noznice');   
-    let hracovVyber = vstup.toLowerCase();
-    let pocitacVyber = computerPlay() 
-    let robot = alert('Počítač si vybral: ' + pocitacVyber);
-if (hracovVyber === pocitacVyber) {
-    alert('Remíza')
-} else if (hracovVyber == 'kamen' && pocitacVyber == 'papier' ) {
-    alert('Prehrali ste, papier poráže kameň')
-  pocitac += 1 
-} else if (hracovVyber == 'papier' && pocitacVyber == 'kamen' ) {
-    alert('Vyhrali ste, papier poráža kameň')
-    hrac += 1 
-} else if (hracovVyber == 'noznice' && pocitacVyber == 'papier' ) {
-    alert('Vyhrali ste, nožnice porážajú papier')
-    hrac += 1 
-} else if (hracovVyber == 'papier' && pocitacVyber == 'noznice' ) {
-    alert('Prehrali ste, nožnice porážajú papier')
-    pocitac += 1 
-} else if (hracovVyber == 'noznice' && pocitacVyber == 'kamen' ) {
-    alert('Prehrali ste, kameň poráža nožnice')
-    pocitac += 1 
-} else if (hracovVyber == 'kamen' && pocitacVyber == 'noznice' ) {
-    alert('Vyhrali ste, kameň poráža nožnice')
-    hrac += 1 
-}
+
+
+
+function kamen() {
+    let pocitacVyber = computerPlay()
+    if(pocitacVyber == 'kamen') {
+        document.getElementById("hodnotenie").innerHTML = "Remíza";
+    } else if (pocitacVyber == 'papier') {
+        document.getElementById("hodnotenie").innerHTML = "Prehrali ste ";
+        pocitac += 1
+    }
+    else if(pocitacVyber == 'noznice') {
+        document.getElementById("hodnotenie").innerHTML = "Vyhrali ste ";
+        hrac += 1
+    }
+    document.getElementById("skoreH").innerHTML = hrac;
+document.getElementById("skoreP").innerHTML = pocitac;
+rozhodca()
 }
 
-function Hra() {
-let pocitadlo = 1    
-while (pocitadlo < 6) {
- Kolo();
- pocitadlo += 1
-}  
-if (hrac > pocitac ) {
-    alert('Vyhrali ste viac hier a preto ste absolútnym výťazom!')
-} else if (hrac < pocitac) {
-    alert('Počítač Vás porazil, nabudúce budete mať možno viacej šťastia') }
- else if (hrac == pocitac) 
- {
-    alert('Stalo sa nemožné a nastala remíza :D')
-} else {
-    alert('ja som jahoda')
+function papier() {
+    let pocitacVyber = computerPlay()
+    if(pocitacVyber == 'kamen') {
+        document.getElementById("hodnotenie").innerHTML = "Vyhrali ste";
+        hrac += 1
+    } else if (pocitacVyber == 'papier') {
+        document.getElementById("hodnotenie").innerHTML = "Remíza ";
+    }
+    else if(pocitacVyber == 'noznice') {
+        document.getElementById("hodnotenie").innerHTML = "Prehrali ste ";
+        pocitac += 1
+    }
+    document.getElementById("skoreH").innerHTML = hrac;
+document.getElementById("skoreP").innerHTML = pocitac;
 }
-alert(hrac)
-alert(pocitac)
+
+function noznice() {
+    let pocitacVyber = computerPlay()
+    if(pocitacVyber == 'kamen') {
+        document.getElementById("hodnotenie").innerHTML = "Prehrali ste ";
+        pocitac += 1
+    } else if (pocitacVyber == 'papier') {
+        document.getElementById("hodnotenie").innerHTML = "Vyhrali ste ";
+        hrac += 1
+    }
+    else if(pocitacVyber == 'noznice') {
+        document.getElementById("hodnotenie").innerHTML = "Remíza ";
+    }
+    document.getElementById("skoreH").innerHTML = hrac;
+document.getElementById("skoreP").innerHTML = pocitac;
 }
+
+
+function rozhodca() {
+    if(pocitac == 5) {
+        document.getElementById('verdikt').innerHTML = 'Súťaž vyhral počítač!';
+    }
+    else if(hrac == 5) {
+        document.getElementById('verdikt').innerHTML = 'Súťaž ste vyhrali! Gratulujeme!';
+    }
+    else {
+        document.getElementById('verdikt').innerHTML = '';
+    }
+}
+
+
+
+
